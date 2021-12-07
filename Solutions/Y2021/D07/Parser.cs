559ac47f5e9f675f0ac9b66c8505e813c0542cc4
habetuz <marfinn3@gmail.com>
2021-12-07 15:47:18 +0100

    Day 7 Parser

---
+++ Solutions/Y2021/D07/Parser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Common;
+using SharpLog;
+
+namespace AdventOfCode.Solutions.Y2021.D07
+{
+    internal class Parser : Parser<int[]>
+    {
+        internal override int[] Parse(string input)
+        {
+            string[] values = input.Split(',');
+
+            s_progressTracker = new ProgressTracker(values.Length - 1, (int progress) =>
+            {
+                s_logger.Log(ProgressTracker.ProgressToString(progress), LogType.Info);
+            });
+
+            List<int> startValues = new List<int>();
+
+            foreach (string value in values)
+            {
+                startValues.Add(int.Parse(value));
+
+                s_progressTracker.CurrentStep++;
+            }
+
+            return startValues.ToArray();
+        }
+    }
+}
---
+++ Solutions/Y2021/D07/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions.Y2021.D07
+{
+    internal class Solution : Solution<int[]>
+    {
+        internal override string Puzzle1(int[] input)
+        {
+            return base.Puzzle1(input);
+        }
+    }
+}
