ee129917a5e22c03ae2ffcf458094e7b50fec757
habetuz <marfinn3@gmail.com>
2021-12-13 14:15:12 +0100

    Day 9 Parser

---
+++ Solutions/Y2021/D09/Parser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Common;
+using SharpLog;
+
+namespace AdventOfCode.Solutions.Y2021.D09
+{
+    internal class Parser : Parser<int[,]>
+    {
+        internal override int[,] Parse(string input)
+        {
+            string[] values = input.Split(new char[] {'\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+            s_progressTracker = new ProgressTracker(values.Length, (int progress) =>
+            {
+                s_logger.Log(ProgressTracker.ProgressToString(progress), LogType.Info);
+            });
+
+            int[,] output = new int[values[0].Length, values.Length];
+
+            for (int y = 0; y < output.GetLength(1); y++)
+            {
+                for (int x = 0; x < output.GetLength(0); x++)
+                {
+                    output[x, y] = values[y][x];
+                }
+
+                s_progressTracker.CurrentStep++;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Solutions/Y2021/D09/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions.Y2021.D09
+{
+    internal class Solution : Solution<int[,]>
+    {
+        
+    }
+}
