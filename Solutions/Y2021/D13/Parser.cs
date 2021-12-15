bc823c7f12bfdff8ceedf72d3be4a04cb968133c
Marvin Fuchs <marfinn3@gmail.com>
2021-12-15 17:47:53 +0100

    Day 12

---
+++ Solutions/Y2021/D13/Parser.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions.Y2021.D13
+{
+    internal class Parser : Parser<((char, int)[], bool[,])>
+    {
+        internal override ((char, int)[], bool[,]) Parse(string input)
+        {
+            string[] lines = input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            SetupProgressTracker(lines.Length + 1);
+
+            List<(char, int)> instructions = new List<(char, int)>();
+            List<(int, int)> coordinates = new List<(int, int)>();
+            (int, int) maxIndex = (0, 0);
+
+            foreach (string line in lines)
+            {
+                if (line[0] == 'f')
+                {
+                    instructions.Add((line[11], int.Parse(line.Substring(13))));
+                }
+                else
+                {
+                    coordinates.Add((int.Parse(line.Split(',')[0]), int.Parse(line.Split(',')[1])));
+                    if (coordinates.Last().Item1 > maxIndex.Item1) maxIndex.Item1 = coordinates.Last().Item1;
+                    if (coordinates.Last().Item2 > maxIndex.Item2) maxIndex.Item2 = coordinates.Last().Item2;
+                }
+                s_progressTracker.CurrentStep++;
+            }
+
+            bool[,] paper = new bool[maxIndex.Item1 + 1, maxIndex.Item2 + 1];
+
+            foreach ((int, int) coordinate in coordinates)
+            {
+                paper[coordinate.Item1, coordinate.Item2] = true;
+            }
+
+            return (instructions.ToArray(), paper);
+        }
+    }
+}
