d759c26fe99fb2c0024e62bfb35305f68a700f52
Marvin Fuchs <marfinn3@gmail.com>
2022-03-13 13:58:06 +0100

    Start Day 22

---
+++ Solutions/Y2021/D22/Parser.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions.Y2021.D22
+{
+    internal class Parser : Parser<(bool, (int, int, int), (int, int, int))[]>
+    {
+        internal override (bool, (int, int, int), (int, int, int))[] Parse(string input)
+        {
+            string[] lines = input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            (bool, (int, int, int), (int, int, int))[] instructions = new (bool, (int, int, int), (int, int, int))[lines.Length];
+
+            for(int i = 0; i < instructions.Length; i++)
+            {
+                string line = lines[i];
+                bool on = line[2] == 'n'? true : false;
+
+                string[] parts = line.Split(new string[] { "=", "..", "," }, StringSplitOptions.None);
+
+                (int, int, int) from = (Int32.Parse(parts[1]), Int32.Parse(parts[4]), Int32.Parse(parts[7]));
+                (int, int, int) to = (Int32.Parse(parts[2]), Int32.Parse(parts[5]), Int32.Parse(parts[8]));
+
+                instructions[i] = (on, from, to);
+            }
+
+            return instructions;
+        }
+    }
+}
---
+++ Solutions/Y2021/D22/Solution.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions.Y2021.D22
+{
+
+    internal class Solution : Solution<(bool, (int, int, int), (int, int, int))[]>
+    {
+        const int HalfMax = int.MaxValue / 2;
+
+        internal override string Puzzle1((bool, (int, int, int), (int, int, int))[] input)
+        {
+            bool[,,] reactor = new bool[int.MaxValue, int.MaxValue, int.MaxValue];
+
+            return base.Puzzle1(input);
+        }
+
+        private bool GetValue(int x, int y, int z, bool[,,] reactor)
+        {
+            return reactor[HalfMax + x, HalfMax + y, HalfMax + z];
+        }
+
+        private void SetValue(bool value, int x, int y, int z, ref bool[,,] reactor)
+        {
+            reactor[HalfMax + x, HalfMax + y, HalfMax + z] = value;
+        }
+    }
+}
