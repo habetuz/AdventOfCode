875f5e07a9ccfb03cffc41180b58d7f4c4879cd5
Marvin Fuchs <marfinn3@gmail.com>
2022-03-14 18:01:18 +0100

    Starting Day 23

---
+++ Solutions/Y2021/D23/Parser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions.Y2021.D23
+{
+    internal class Parser : Parser<char[,]>
+    {
+        internal override char[,] Parse(string input)
+        {
+            string[] lines = input.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+            char[,] result = new char[lines[0].Length ,lines.Length];
+
+            for (int y = 0; y < lines.Length; y++)
+            {
+                for (int x = 0; x < lines[0].Length; x++)
+                {
+                    try
+                    {
+                        result[x, y] = lines[y][x];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        result[x, y] = ' ';
+                    }
+                }
+            }
+
+            Tools.Print2D(result);
+
+            return result;
+        }
+    }
+}
---
+++ Solutions/Y2021/D23/Solution.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode.Common;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Solutions.Y2021.D23
+{
+    internal class Solution : Solution<char[,]>
+    {
+        private static readonly (byte, byte) AGoal = (3, 2);
+        private static readonly (byte, byte) BGoal = (5, 2);
+        private static readonly (byte, byte) CGoal = (7, 2);
+        private static readonly (byte, byte) DGoal = (9, 2);
+
+        private static readonly Dictionary<char, (byte, byte)> GoalPos = new Dictionary<char, (byte, byte)>
+        {
+            {'A', AGoal },
+            {'B', BGoal },
+            {'C', CGoal },
+            {'D', DGoal },
+        };
+
+        private static readonly Dictionary<char, ushort> Costs = new Dictionary<char, ushort>
+        {
+            {'A', 1 },
+            {'B', 10 },
+            {'C', 100 },
+            {'D', 1000 },
+        };
+
+        private readonly (byte, byte)[] Positions = new (byte, byte)[]
+        {
+            (1, 1),
+            (2, 1),
+            (4, 1),
+            (6, 1),
+            (8, 1),
+            (10, 1),
+            (11, 1),
+        };
+
+        internal override string Puzzle1(char[,] input)
+        {
+            return base.Puzzle1(input);
+        }
+    }
+}
