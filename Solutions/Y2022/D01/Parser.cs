8ecb8c6a0a0eeea6b0c64ce1d994e3b8919e1bb0
Marvin Fuchs <marfinn3@gmail.com>
2022-12-01 21:44:39 +0100

    Y2022 D01 P01 and P02

---
+++ Solutions/Y2022/D01/Parser.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode.Solutions.Y2022.D01
+{
+    using System.Collections.Generic;
+    using AdventOfCode.Common;
+
+    internal class Parser : Parser<uint[][]>
+    {
+        internal override uint[][] Parse(string input)
+        {
+            var lines = input.Split('\n');
+
+            List<uint[]> inventories = new List<uint[]>();
+
+            List<uint> currentInventory = new List<uint>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                uint calories;
+                if (uint.TryParse(lines[i], out calories)) 
+                {
+                    currentInventory.Add(calories);
+                }
+                else
+                {
+                    inventories.Add(currentInventory.ToArray());
+                    currentInventory.Clear();
+                }
+            }
+
+            inventories.Add(currentInventory.ToArray());
+
+            return inventories.ToArray();
+        }
+    }
+}
---
+++ Solutions/Y2022/D01/Solution.cs
@@ -0,0 +1,66 @@
+﻿namespace AdventOfCode.Solutions.Y2022.D01
+{
+    using AdventOfCode.Common;
+
+    internal class Solution : Solution<uint[][]>
+    {
+        internal override (object clipboard, string message) Puzzle1(uint[][] input)
+        {
+            uint maxCalories = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                uint currentCalories = 0;
+
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    currentCalories += input[i][j];
+                }
+
+                if (currentCalories > maxCalories)
+                {
+                    maxCalories = currentCalories;
+                }
+            }
+
+            return (maxCalories, $"The top elve is carrying {maxCalories} calories!");
+        }
+
+        internal override (object clipboard, string message) Puzzle2(uint[][] input)
+        {
+            uint[] topThree = new uint[3];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                uint currentCalories = 0;
+
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    currentCalories += input[i][j];
+                }
+
+                for (int j = 0; j < topThree.Length; j++)
+                {
+                    if (currentCalories > topThree[j])
+                    {
+                        var toShift = topThree[j];
+                        topThree[j] = currentCalories;
+
+                        for (int k = j + 1; k < topThree.Length; k++)
+                        {
+                            var temp = topThree[k];
+                            topThree[k] = toShift;
+                            toShift = temp;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            var topThreeTogether = topThree[0] + topThree[1] + topThree[2];
+
+            return (topThreeTogether, $"The top three elves are carrying {topThreeTogether} calories!");
+        }
+    }
+}
