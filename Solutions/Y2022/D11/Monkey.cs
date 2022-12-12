ee5408a6adad14426965ba04c2fe981e788ca8cf
Marvin Fuchs <marfinn3@gmail.com>
2022-12-12 23:20:32 +0100

    Y2022 D11 P01

---
+++ Solutions/Y2022/D11/Monkey.cs
@@ -0,0 +1,127 @@
+﻿namespace AdventOfCode.Solutions.Y2022.D11
+{
+    using System.Collections.Generic;
+
+    internal class Monkey
+    {
+        private char operation;
+        private int @operator;
+        private int dividend;
+        private int caseTrue;
+        private int caseFalse;
+
+        internal Monkey(Queue<int> items, char operation, int @operator, int dividend, int caseTrue, int caseFalse)
+        {
+            this.Items = items;
+            this.operation = operation;
+            this.@operator = @operator;
+            this.dividend = dividend;
+            this.caseTrue = caseTrue;
+            this.caseFalse = caseFalse;
+        }
+
+        internal Queue<int> Items { get; set; }
+
+        internal int Dividend { get => this.dividend; }
+
+        internal (int, int) Inspect()
+        {
+            var item = this.Items.Dequeue();
+
+            var @operator = this.@operator == -1 ? item : this.@operator;
+
+            if (this.operation == '*')
+            {
+                item *= @operator;
+            }
+            else
+            {
+                item += @operator;
+            }
+
+            item /= 3;
+
+            if (item % this.dividend == 0)
+            {
+                return (item, this.caseTrue);
+            }
+            else
+            {
+                return (item, this.caseFalse);
+            }
+        }
+
+        internal (int, int) InspectHard(int[] dividends)
+        {
+            var item = this.Items.Dequeue();
+
+            var @operator = this.@operator == -1 ? item : this.@operator;
+
+            if (this.operation == '*')
+            {
+                item *= @operator;
+            }
+            else
+            {
+                item += @operator;
+            }
+
+            // My understanding is that the number has to be the lowest that has the same modulo for all dividends.
+            var values = new int[dividends.Length];
+            var multipliers = new int[dividends.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                multipliers[i] = 1;
+                values[i] = dividends[i] - (item % dividends[i]);
+            }
+
+            bool correct;
+
+            do
+            {
+                correct = true;
+
+                for (int i = 1; i < dividends.Length; i++)
+                {
+                    while (values[i] < values[0])
+                    {
+                        multipliers[i]++;
+                        values[i] = (multipliers[i] * dividends[i]) - (item % dividends[i]);
+                    }
+
+                    if (values[i] != values[0])
+                    {
+                        correct = false;
+                    }
+                }
+
+                multipliers[0]++;
+                values[0] = (multipliers[0] * dividends[0]) - (item % dividends[0]);
+            }
+            while (!correct);
+
+            item = values[1];
+
+            if (item % this.dividend == 0)
+            {
+                return (item, this.caseTrue);
+            }
+            else
+            {
+                item = this.dividend - (item % this.dividend);
+                return (item, this.caseFalse);
+            }
+        }
+
+        internal Monkey Clone()
+        {
+            return new Monkey(
+                new Queue<int>(this.Items),
+                this.operation,
+                this.@operator,
+                this.dividend,
+                this.caseTrue,
+                this.caseFalse);
+        }
+    }
+}
---
+++ Solutions/Y2022/D11/Solution.cs
@@ -0,0 +1,74 @@
+﻿namespace AdventOfCode.Solutions.Y2022.D11
+{
+    using System;
+    using AdventOfCode.Common;
+
+    internal class Solution : Solution<Monkey[]>
+    {
+        internal override (object clipboard, string message) Puzzle1(Monkey[] monkeys)
+        {
+            var monkeysCopy = new Monkey[monkeys.Length];
+            for (int i = 0; i < monkeys.Length; i++)
+            {
+                monkeysCopy[i] = monkeys[i].Clone();
+            }
+
+            monkeys = monkeysCopy;
+
+            int[] inspections = new int[monkeys.Length];
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int m = 0; m < monkeys.Length; m++)
+                {
+                    while (monkeys[m].Items.Count > 0)
+                    {
+                        (var item, var to) = monkeys[m].Inspect();
+                        monkeys[to].Items.Enqueue(item);
+
+                        inspections[m]++;
+                    }
+                }
+            }
+
+            Array.Sort(inspections);
+            Array.Reverse(inspections);
+
+            return (inspections[0] * inspections[1], $"The monkey business is [yellow]{inspections[0] * inspections[1]}[/]!");
+        }
+
+        internal override (object clipboard, string message) Puzzle2(Monkey[] monkeys)
+        {
+            int[] inspections = new int[monkeys.Length];
+
+            var dividends = new int[monkeys.Length];
+
+            for (int i = 0; i < monkeys.Length; i++)
+            {
+                dividends[i] = monkeys[i].Dividend;
+            }
+
+            Array.Sort(dividends);
+            Array.Reverse(dividends);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                for (int m = 0; m < monkeys.Length; m++)
+                {
+                    while (monkeys[m].Items.Count > 0)
+                    {
+                        (var item, var to) = monkeys[m].InspectHard(dividends);
+                        monkeys[to].Items.Enqueue(item);
+
+                        inspections[m]++;
+                    }
+                }
+            }
+
+            Array.Sort(inspections);
+            Array.Reverse(inspections);
+
+            return (inspections[0] * inspections[1], $"The monkey business is [yellow]{inspections[0] * inspections[1]}[/]!");
+        }
+    }
+}
