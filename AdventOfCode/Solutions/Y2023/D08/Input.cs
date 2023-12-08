1203f78d93cf9b7c38862a964df44e940a4c58e1
Marvin Fuchs <info@marvin-fuchs.de>
2023-12-08 08:44:37 +0100

    Y2023 D08

---
+++ AdventOfCode/Solutions/Y2023/D08/Input.cs
@@ -0,0 +1,15 @@
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Solutions.Y2023.D08;
+
+public struct Input
+{
+    public Direction[] DirectionSequence { get; }
+    public BiNode<string>[] Nodes { get; }
+
+    public Input(Direction[] directionSequence, BiNode<string>[] nodes)
+    {
+        DirectionSequence = directionSequence;
+        Nodes = nodes;
+    }
+}
---
+++ AdventOfCode/Utils/BiNode.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+
+namespace AdventOfCode.Utils;
+
+[DebuggerDisplay("Value = {Value}")]
+public class BiNode<T>
+{
+    public T Value { get; set; }
+    public BiNode<T>? Left { get; set; }
+    public BiNode<T>? Right { get; set; }
+
+    public BiNode(T value, BiNode<T>? left = null, BiNode<T>? right = null)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+    }
+
+    public BiNode<T>? GetDirection(Direction direction) =>
+        direction switch
+        {
+            Direction.Left => Left,
+            Direction.Right => Right,
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
+}
